099a4be3fabc982c93d31b5209ec8d5fff984c2b
thiizz <afffvitu@gmail.com>
2023-07-11 02:17:16 -0300

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,20 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { Content } from "~/components/content";
 import { Hero } from "~/components/hero";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "Plahiz" },
+    { title: "ZihtFlix" },
     { name: "description", content: "Welcome to Remix!" },
-
   ];
 };
 
 export default function Index() {
+
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Content/>
+    </>
   );
 }
